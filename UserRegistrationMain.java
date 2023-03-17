177a1b07dc86added107768feab260b6f117f916
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-17 20:54:30 +0530

    Added & modified : code for to validate last name

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,10 +1,37 @@
 package regex_project;
 
+import java.util.Scanner;
+
 public class UserRegistrationMain {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         UserValidationFunctions validationFunctions = new UserValidationFunctions();
         System.out.println("Welcome to Validation Program");
-        validationFunctions.checkFirstName("Ajay");
+
+
+        boolean loop = true;
+        while (loop) {
+            System.out.println("Enter which user information you want to validate");
+            System.out.println("1. first name \n 2. Last name \n 3. exits");
+            int input = sc.nextInt();
+            final int firstName = 1, lastName = 2 , exit = 3;
+
+            switch (input) {
+                case firstName :
+                    validationFunctions.checkFirstName("Ajay");
+                    break;
+                case  lastName:
+                    validationFunctions.checkLastName();
+                    break;
+                case exit:
+                    loop = false;
+                    System.out.println(" Thank for visiting ");
+                    break;
+                default:
+                    System.out.println("Invalid input ....");
+                    break;
+            }
+        }
     }
 }
--- UserValidationFunctions.java
+++ UserValidationFunctions.java
@@ -1,5 +1,6 @@
 package regex_project;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,4 +16,19 @@ public class UserValidationFunctions {
             System.out.println("Sorry! you have entered invalid First Name");
         }
     }
+
+    public void checkLastName() {
+        // uc2 - validate last name
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Last Name : "); //Pattern.matches("^[A-Z][a-z]{2,}$", firstName);
+        System.out.println("Last name starts with Capital letters and has minimum 3 characters");
+        String lastName = scanner.next();
+        boolean result = Pattern.matches("^[A-Z][a-z]{2,}$", lastName);
+        if (result) {
+            System.out.println("You have entered valid LastName ");
+        } else {
+            System.out.println("Sorry! you have entered invalid Last Name");
+        }
+    }
+
 }
